f68b17b79e2f4deb2d31775e0996efb719a70cc2
TAMTARA <karenpbq@gmail.com>
2026-07-06 18:18:35 -0500

    Change initial_sidebar_state to 'auto'

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from tracking import mostrar_modulo_tracking
 st.set_page_config(
     page_title="La Exacta",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="auto",
     page_icon=".streamlit/static/logo.png"
 )
 
