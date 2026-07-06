e3f6588552b9157a83c1b6938239fd5f5f77c692
TAMTARA <karenpbq@gmail.com>
2026-07-06 13:27:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from carta import mostrar_modulo_carta
 
 # --- CONFIGURACIÓN DE IDENTIDAD VISUAL EN LA NUBE ---
 # Apuntamos a la URL directa del logo subido en tu repositorio de GitHub
-URL_LOGO_LA_EXACTA = "https://raw.githubusercontent.com/tamtara-hbgs/main/Logo_La_Exacta_1.jpg"
+URL_LOGO_LA_EXACTA = "https://raw.githubusercontent.com/tamtara-hbgs/main/Logo.png"
 
 # 1. Configuración de página (MODIFICADO: Tu logo real en la pestaña del navegador)
 st.set_page_config(
@@ -33,7 +33,7 @@ if autenticado:
         # AQUÍ ES DONDE LIMPIAMOS EL CONTENIDO CENTRAL
         if menu == "Inicio":
             st.header("👑 Panel de Control")
-            st.info("Bienvenida al centro de mando de HBGS. Usa el menú lateral para gestionar tu negocio.")
+            st.info("Bienvenida al centro de mando de La Exacta. Usa el menú lateral para gestionar tu negocio.")
         
         elif menu == "Costos (Insumos)":
             mostrar_modulo_costos()
